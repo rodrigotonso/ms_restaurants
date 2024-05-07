a3b8f195015a212849aa0c72b40ba6ffadf46329
Rodrigo Tonso <67134489+rodrigotonso@users.noreply.github.com>
2024-05-07 00:47:54 -0300

    add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
@@ -6,6 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+
+  const config = new DocumentBuilder()
+    .setTitle('Restaurants')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(port);
 }
